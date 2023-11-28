5fd010343944134b9775890f1c4371e3dfdc49ce
Wat (Welliton A. Toledo) <wat@actuar.com>
2023-11-28 17:51:50 -0300

    EnumExtensions

--- src/Toletus.Pack.Core/Extensions/EnumExtensions.cs
+++ src/Toletus.Pack.Core/Extensions/EnumExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using EnumsNET;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Toletus.Pack.Core.Extensions;
 
@@ -24,9 +25,44 @@ public static class EnumExtensions
     {
         return Enum.GetNames(@this.GetType());
     }
-    
-    public static string? GetDescription<T>(this T @this) where T : struct, Enum
+
+    // public static string GetDescription<T>(this T @this) where T : struct, Enum
+    // {
+    //     return @this.AsString(EnumFormat.Description);
+    // }
+
+    public static string GetDescription(this Enum value)
     {
-        return @this.AsString(EnumFormat.Description);
+        if (value == null) return null;
+
+        var field = value.GetType().GetField(value.ToString());
+
+        var attribute = field?.GetCustomAttributes(typeof(DescriptionAttribute), false)
+            .SingleOrDefault() as DescriptionAttribute;
+
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+
+    public static T GetValueFromDescription<T>(this string description)
+    {
+        var type = typeof(T);
+        if (!type.IsEnum) throw new InvalidOperationException();
+        foreach (var field in type.GetFields())
+        {
+            var attribute = Attribute.GetCustomAttribute(field,
+                typeof(DescriptionAttribute)) as DescriptionAttribute;
+            if (attribute != null)
+            {
+                if (attribute.Description == description)
+                    return (T)field.GetValue(null);
+            }
+            else
+            {
+                if (field.Name == description)
+                    return (T)field.GetValue(null);
+            }
+        }
+
+        return default(T);
     }
 }
