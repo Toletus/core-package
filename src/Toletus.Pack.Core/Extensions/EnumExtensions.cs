55639695b55ca5e466a03734461f3c33d44e6c84
Wat (Welliton A. Toledo) <wat@actuar.com>
2023-11-28 17:45:41 -0300

    EnumExtensions

---
+++ src/Toletus.Pack.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EnumsNET;
+
+namespace Toletus.Pack.Core.Extensions;
+
+public static class EnumExtensions
+{
+    public static bool In(this Enum @this, params Enum[] values)
+    {
+        return Array.IndexOf(values, @this) != -1;
+    }
+
+    public static bool NotIn(this Enum @this, params Enum[] values)
+    {
+        return Array.IndexOf(values, @this) == -1;
+    }
+
+    public static T Parse<T>(this Enum @this) where T : Enum
+    {
+        return (T)Enum.Parse(typeof(T), @this.ToString());
+    }
+
+    public static string[] GetNames(this Enum @this)
+    {
+        return Enum.GetNames(@this.GetType());
+    }
+    
+    public static string? GetDescription<T>(this T @this) where T : struct, Enum
+    {
+        return @this.AsString(EnumFormat.Description);
+    }
+}
