1e64165400756172f532dc5951a8cf14c9e8c37f
Edson Santos <edson@actuar.com>
2025-07-14 18:05:13 -0300

    feat: adicionados IpConfig e IpMode ao projeto Core
    Incluídos os modelos `IpConfig` e `IpMode` no namespace `Toletus.Pack.Core`, fornecendo suporte para configurações de IP dinâmico e fixo.

---
+++ src/Toletus.Pack.Core/Enums/IpMode.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Toletus.Pack.Core.Enums;
+
+public enum IpMode
+{
+    [Description("Dynamic (DHCP)")]
+    Dynamic,
+    [Description("Fixed")]
+    Fixed   
+}
---
+++ src/Toletus.Pack.Core/IpConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Toletus.Pack.Core.Enums;
+
+namespace Toletus.Pack.Core;
+
+public class IpConfig
+{
+    public IpMode? IpMode { get; set; }
+    public IPAddress? FixedIp { get; set; }
+    public IPAddress? SubnetMask { get; set; } 
+}
