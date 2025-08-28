d0803c9bf9523efcdd32289924cf914d1ab602af
Wat (Welliton A. Toledo) <wat@actuar.com>
2025-08-28 20:02:13 -0300

    refactor: change namespace declaration to file-scoped across Extensions classes

--- src/Toletus.Pack.Core/Extensions/ExceptionExtensions.cs
+++ src/Toletus.Pack.Core/Extensions/ExceptionExtensions.cs
@@ -3,78 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Toletus.Pack.Core.Extensions
+namespace Toletus.Pack.Core.Extensions;
+
+public static class ExceptionExtensions
 {
-    public static class ExceptionExtensions
+    /// <summary>
+    ///  Provides full stack trace for the exception that occurred.
+    /// </summary>
+    /// <param name="exception">Exception object.</param>
+    /// <param name="environmentStackTrace">Environment stack trace, for pulling additional stack frames.</param>
+    public static string ToLogString(this Exception exception, string environmentStackTrace)
     {
-        /// <summary>
-        ///  Provides full stack trace for the exception that occurred.
-        /// </summary>
-        /// <param name="exception">Exception object.</param>
-        /// <param name="environmentStackTrace">Environment stack trace, for pulling additional stack frames.</param>
-        public static string ToLogString(this Exception exception, string environmentStackTrace)
+        var environmentStackTraceLines =
+            GetUserStackTraceLines(environmentStackTrace);
+
+        var messages = exception.MessagesToString();
+
+        var logMessage = messages;
+
+        if (environmentStackTraceLines.Count <= 0) return logMessage;
+
+        try
         {
-            var environmentStackTraceLines =
-                GetUserStackTraceLines(environmentStackTrace);
+            environmentStackTraceLines.RemoveAt(0);
 
-            var messages = exception.MessagesToString();
+            var stackTraceLines = GetStackTraceLines(exception.StackTrace!);
+            stackTraceLines.AddRange(environmentStackTraceLines);
 
-            var logMessage = messages;
+            var fullStackTrace = string.Join(Environment.NewLine, stackTraceLines);
 
-            if (environmentStackTraceLines.Count <= 0) return logMessage;
-
-            try
-            {
-                environmentStackTraceLines.RemoveAt(0);
-
-                var stackTraceLines = GetStackTraceLines(exception.StackTrace!);
-                stackTraceLines.AddRange(environmentStackTraceLines);
-
-                var fullStackTrace = string.Join(Environment.NewLine, stackTraceLines);
-
-                logMessage += Environment.NewLine + fullStackTrace;
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-
-            return logMessage;
+            logMessage += Environment.NewLine + fullStackTrace;
+        }
+        catch (Exception)
+        {
+            // ignored
         }
 
-        public static string MessagesToString(this Exception ex)
+        return logMessage;
+    }
+
+    public static string MessagesToString(this Exception ex)
+    {
+        var ret = ex.Message;
+
+        while (ex.InnerException != null)
         {
-            var ret = ex.Message;
-
-            while (ex.InnerException != null)
-            {
-                ret += Environment.NewLine + ex.InnerException.Message;
-                ex = ex.InnerException;
-            }
-
-            return ret;
+            ret += Environment.NewLine + ex.InnerException.Message;
+            ex = ex.InnerException;
         }
 
-        /// <summary>
-        ///  Gets a list of stack frame lines, as strings.
-        /// </summary>
-        /// <param name="stackTrace">Stack trace string.</param>
-        private static List<string> GetStackTraceLines(string stackTrace)
-        {
-            return stackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-        }
+        return ret;
+    }
 
-        /// <summary>
-        ///  Gets a list of stack frame lines, as strings, only including those for which line number is known.
-        /// </summary>
-        /// <param name="fullStackTrace">Full stack trace, including external code.</param>
-        private static List<string> GetUserStackTraceLines(string fullStackTrace)
-        {
-            var regex = new Regex(@"([^\)]*\)) in (.*):line (\d)*$");
+    /// <summary>
+    ///  Gets a list of stack frame lines, as strings.
+    /// </summary>
+    /// <param name="stackTrace">Stack trace string.</param>
+    private static List<string> GetStackTraceLines(string stackTrace)
+    {
+        return stackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+    }
 
-            var stackTraceLines = GetStackTraceLines(fullStackTrace);
+    /// <summary>
+    ///  Gets a list of stack frame lines, as strings, only including those for which line number is known.
+    /// </summary>
+    /// <param name="fullStackTrace">Full stack trace, including external code.</param>
+    private static List<string> GetUserStackTraceLines(string fullStackTrace)
+    {
+        var regex = new Regex(@"([^\)]*\)) in (.*):line (\d)*$");
 
-            return stackTraceLines.Where(stackTraceLine => regex.IsMatch(stackTraceLine)).ToList();
-        }
+        var stackTraceLines = GetStackTraceLines(fullStackTrace);
+
+        return stackTraceLines.Where(stackTraceLine => regex.IsMatch(stackTraceLine)).ToList();
     }
 }
--- src/Toletus.Pack.Core/Extensions/StringExtensions.cs
+++ src/Toletus.Pack.Core/Extensions/StringExtensions.cs
@@ -1,35 +1,34 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Toletus.Pack.Core.Extensions
+namespace Toletus.Pack.Core.Extensions;
+
+public static class StringExtensions
 {
-    public static class StringExtensions
+    public static string Truncate(this string input, int maxLength)
     {
-        public static string Truncate(this string input, int maxLength)
-        {
-            if (string.IsNullOrEmpty(input)) return input;
-            return input.Length <= maxLength ? input : input.Substring(0, maxLength);
-        }
+        if (string.IsNullOrEmpty(input)) return input;
+        return input.Length <= maxLength ? input : input.Substring(0, maxLength);
+    }
 
-        private static readonly Regex WhitespaceRegex = new(@"\s+");
+    private static readonly Regex WhitespaceRegex = new(@"\s+");
         
-        public static string ReplaceWhitespace(this string input, string replacement)
-        {
-            return WhitespaceRegex.Replace(input, replacement);
-        }
+    public static string ReplaceWhitespace(this string input, string replacement)
+    {
+        return WhitespaceRegex.Replace(input, replacement);
+    }
 
-        public static string RemoveWhitespace(this string input)
-        {
-            return WhitespaceRegex.Replace(input, string.Empty);
-        }
+    public static string RemoveWhitespace(this string input)
+    {
+        return WhitespaceRegex.Replace(input, string.Empty);
+    }
 
-        public static string RemoveDiacritics(this string input)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var tempBytes = Encoding.GetEncoding("ISO-8859-8").GetBytes(input);
-            var asciiStr = Encoding.UTF8.GetString(tempBytes);
+    public static string RemoveDiacritics(this string input)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        var tempBytes = Encoding.GetEncoding("ISO-8859-8").GetBytes(input);
+        var asciiStr = Encoding.UTF8.GetString(tempBytes);
 
-            return asciiStr;
-        }
+        return asciiStr;
     }
 }
